07409d3614b4f13a9a13d4b87db49c0caf129243
anbumanidev <anbumani.dsaidev@gmail.com>
2024-08-07 22:08:53 +0530

    anbu1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 app=Flask(__name__)
 ## load the model
 regmodel=pickle.load(open("regmodel.pkl",'rb'))
-scalar=pickle.load(open('scalling.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -26,7 +26,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    final_input=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input[0])
     return render_template("home.html",prediction_text="the House price prediction is {}".format(output))
